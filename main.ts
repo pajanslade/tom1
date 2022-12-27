ab82be7fe59e3383ab6713f0b60fd6fd470f0fda
pajanslade <121522335+pajanslade@users.noreply.github.com>
2022-12-27 23:03:24 +0100

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+for (let index = 0; index < 19; index++) {
+    music.playMelody("F G E D F A B G ", 120)
+    music.playMelody("F G E D F A C5 B ", 120)
+    music.playMelody("C D E F G E D C ", 120)
+    music.playMelody("C5 A F E F G B A ", 120)
+    music.playMelody("C5 A B G B C5 F C5 ", 120)
+    music.playMelody("A E D F C C5 C F ", 120)
+    music.playMelody("C E D F E D C C ", 120)
+    music.playMelody("C D E C F G A F ", 120)
+}
